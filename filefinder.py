285fd94d3056a7a29dbddaf7881777a0d0cdedb3
LOTWF <47356473+LOTWF@users.noreply.github.com>
2021-04-12 14:44:33 +0100

    filefinder-1.0
    initial commit

---
+++ filefinder.py
@@ -0,0 +1,14 @@
+import os
+'''
+parameters:
+root=root folder to search from
+substr=substring for file, can be extensions as well e.g. '.png'
+exception=optional argument to esxclude files located in specific subpaths (e.g. 'bkp' will escludes files located anywhere with 'bkp' in the filepath from root)
+'''
+def filefinder(root,substr,exception=''):
+    filelst=[]
+    for i in os.walk(root):
+        for j in i[2]:
+            if substr in j and exception not in i[0]:
+                filelst.append(i[0]+os.sep+j)
+    return filelst
